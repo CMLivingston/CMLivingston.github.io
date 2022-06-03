9f41f8693aeffcd70ce2688f1a30dae3597d7373
Chris Livingston <clivingston45@gmail.com>
2022-06-03 12:16:07 -0700

    Updates

---
+++ precache-manifest.9bc52d754596f2678b842a4c7ec02429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4080aa27acc5b8b8f69",
+    "url": "/static/css/main.ebd73403.chunk.css"
+  },
+  {
+    "revision": "d4080aa27acc5b8b8f69",
+    "url": "/static/js/main.d4080aa2.chunk.js"
+  },
+  {
+    "revision": "26ccb0c19ce2957b908b",
+    "url": "/static/js/1.26ccb0c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ed41fa813c44b9a68da31dbda05db6a",
+    "url": "/index.html"
+  }
+];
